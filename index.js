97a9271e710e294eee8ce973bb3461445653024a
Sergey Burdin <senik1806@gmail.com>
2023-09-02 23:09:09 +0300

    Added my project

---
+++ index.js
@@ -0,0 +1,30 @@
+const qrcode = require('qrcode-terminal');
+
+let q = 0
+
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const client = new Client(
+    {authStrategy: new LocalAuth()}
+);
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', message => {
+    console.log('message', message)
+    if (message.from === '79884054121@c.us') {
+        client.sendMessage('79504460593@c.us', 'pong');
+    }
+    q++;
+	// console.log(message.body);
+    // message.reply('Сама ты ' + message.body + q.toString());
+});
+ 
+
+client.initialize();
+ 
