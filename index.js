989243e384075c8e822d2a355e4be3ba13a64b42
Sergey Burdin <senik1806@gmail.com>
2023-09-21 11:42:39 +0300

    190923

--- index.js
+++ index.js
@@ -228,7 +228,8 @@ app.post('/file', upload.single('message_file'), function (req, res, next) {
     
     
     const media = MessageMedia.fromFilePath('./public/static/uploads/message_file-1695284764088.jpg');
-    client.sendMessage(superadmin, media);
+    console.log('media', media)
+    // client.sendMessage(superadmin, media);
     res.send({res: 'success!'})
 })
 // v8
