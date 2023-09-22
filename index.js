9f8681c0c1129cfc31f16f968e84cbef94a40030
Sergey Burdin <senik1806@gmail.com>
2023-09-22 10:19:44 +0300

    190923

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ const express = require('express')
 var cors = require('cors')
 const app = express()
 app.use(cors())
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
