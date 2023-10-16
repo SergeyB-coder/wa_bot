a9f9c48ef3be1a10aec09d2efb5ec09389f056c2
Sergey Burdin <senik1806@gmail.com>
2023-10-16 11:41:08 +0300

    1110

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './public/static/uploads/test');
+        cb(null, './public/static/uploads');
     },
     filename: function (req, file, cb) {
         //   const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
