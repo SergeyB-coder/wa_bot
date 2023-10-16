24f983e510b6c0cc0fbfcb0565a51a3da9242004
Sergey Burdin <senik1806@gmail.com>
2023-10-16 10:36:18 +0300

    1110

--- index.js
+++ index.js
@@ -609,7 +609,7 @@ app.post('/', upload.single('avatar'), function (req, res, next) {
 
 app.post('/file', upload.single('message_file'), function (req, res, next) {
     console.log('req', req)
-    const file = req.file;
+    const file = req.body.message_file;
     const chat_id = parseInt(req.body.chat_id)
     console.log('file: ', file.filename, chat_id, chat_id === 9)
 
