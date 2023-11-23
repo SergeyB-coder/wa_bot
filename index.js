b9d907d7c1b23a30bd5d0f5e492a51788cc2884d
Sergey Burdin <senik1806@gmail.com>
2023-11-23 12:01:03 +0300

    2311

--- index.js
+++ index.js
@@ -165,7 +165,7 @@ function sendServerOn() {
         });
 }
 
-function getContentXlsx() {
+function getContentXlsx(filePath) {
     let workbook = XLSX.readFile( filePath );
     let sheet_name_list = workbook.SheetNames;
     let xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
